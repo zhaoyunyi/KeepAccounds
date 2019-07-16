d37f2e4bb43a73dbd89385e7d8c9bef0af8ea6c8
zhaoyunyi <zcloudswing@gmail.com>
2019-07-16 16:15:46 +0800

    增加价格列表组建

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import PriceList from './components/PriceList';
+
+const items = [
+  {
+    "id": 1,
+    "title": "购买SSD硬盘",
+    "price": 699,
+    "date": "2019-7-12",
+    "category": {
+      "id": "1",
+      "name": "电脑",
+      "type": "outcome",
+    }
+  },
+  {
+    "id": 2,
+    "title": "农信人力服务外包",
+    "price": 650,
+    "date": "2019-6-24",
+    "category": {
+      "id": "2",
+      "name": "外包服务",
+      "type": "income",
+    }
+  },
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*<header className="App-header">*/}
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        {/*<p>*/}
+          {/*Edit <code>src/App.js</code> and save to reload.*/}
+        {/*</p>*/}
+        {/*<a*/}
+          {/*className="App-link"*/}
+          {/*href="https://reactjs.org"*/}
+          {/*target="_blank"*/}
+          {/*rel="noopener noreferrer"*/}
+        {/*>*/}
+          {/*Learn React*/}
+        {/*</a>*/}
+      {/*</header>*/}
+      <PriceList
+          items={items}
+          onModifyItem={(item) => {
+            alert(item.id)
+          }}
+          onDeleteItem={(item) => {
+            alert(item.id)
+          }}
+      />
     </div>
   );
 }
